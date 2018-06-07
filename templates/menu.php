e91602b00491c4356eaab446f09885457a2152a3
Ahmad <ahmad.zeidan@outlook.fr>
2018-06-07 14:08:28 +0200

    add header and footer to templates

---
+++ templates/menu.php
@@ -0,0 +1,45 @@
+<header id="fh5co-header-section" class="sticky-banner">
+    <div class="container">
+        <div class="nav-header">
+            <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
+            <h1 id="fh5co-logo"><a href="index.html"><i class="icon-airplane"></i>Travel</a></h1>
+            <!-- START #fh5co-menu-wrap -->
+            <nav id="fh5co-menu-wrap" role="navigation">
+                <ul class="sf-menu" id="fh5co-primary-menu">
+                    <li class="active"><a href="index.html">Accueil</a></li>
+
+                    <li>
+                        <a href="vacation.html" class="fh5co-sub-ddown">Omra</a>
+                        <ul class="fh5co-sub-menu">
+                            <li><a href="#">Omra économique</a></li>
+                            <li><a href="#">Omra deluxe</a></li>
+                            <li><a href="#">Omra vip sur mesure</a></li>
+                        </ul>
+                    </li>
+
+
+                    <li>
+                        <a href="vacation.html" class="fh5co-sub-ddown">Sejour Ethic</a>
+                        <ul class="fh5co-sub-menu">
+                            <li><a href="#">Malaisie</a></li>
+                            <li><a href="#">Dubai</a></li>
+                            <li><a href="#">Maroc</a></li>
+                            <li><a href="#">Algérie</a></li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a href="vacation.html" class="fh5co-sub-ddown">Information générale</a>
+                        <ul class="fh5co-sub-menu">
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Centres de vaccination</a></li>
+                            <li><a href="#">Documents à fournir</a></li>
+                            <li><a href="#">Cours sur les rites du <br>hajj & Omra</a></li>
+                            <li><a href="#">Avis & témoignages</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
